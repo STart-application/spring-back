a9823d68e01a5f411992e6a5a96957fb6aa37376
9pruruluru2 <9pruruluru2@naver.com>
2023-04-20 18:26:31 +0900

    feat: sse 투표한 유저만 조회 가능하도록 수정

--- src/main/java/seoultech/startapp/festival/adapter/in/VoteController.java
+++ src/main/java/seoultech/startapp/festival/adapter/in/VoteController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import seoultech.startapp.festival.adapter.in.dto.RegisterVoterRequest;
 import seoultech.startapp.festival.application.port.in.GetVoteUseCase;
+import seoultech.startapp.festival.application.port.in.GetVoterUseCase;
 import seoultech.startapp.festival.application.port.in.RegisterVoterUseCase;
 import seoultech.startapp.global.common.SseEmitters;
 import seoultech.startapp.global.config.web.AuthMember;
@@ -28,38 +29,58 @@ public class VoteController {
   private final GetVoteUseCase getVoteUseCase;
   private final RegisterVoterUseCase registerVoterUseCase;
 
+  private final GetVoterUseCase getVoterUseCase;
+
   private final SseEmitters sseEmitters;
 
+  //  @GetMapping("")
+//  public ResponseEntity<?> getVoteList(@LoginMember AuthMember member) {
+//    var result = getVoteUseCase.getVoteSummaryList(member.getMemberId());
+//    return JsonResponse.okWithData(HttpStatus.OK, "투표 전체 조회", result);
+//  }
+//
+//  @GetMapping("/{votingId}")
+//  public ResponseEntity<?> getVoteSummaryResponse(@PathVariable Long votingId, @LoginMember AuthMember member) {
+//    var result = getVoteUseCase.getVoteSummary(votingId, member.getMemberId());
+//    return JsonResponse.okWithData(HttpStatus.OK, "투표 세부 사항 조회", result);
+//  }
+//
+//  @PostMapping("")
+//  public ResponseEntity<?> vote(@RequestBody RegisterVoterRequest voterRequest, @LoginMember AuthMember member) {
+//    registerVoterUseCase.registerVoter(voterRequest.toCommand(member.getMemberId()));
+//    return JsonResponse.ok(HttpStatus.CREATED, "투표 성공");
+//  }
   @GetMapping("")
-  public ResponseEntity<?> getVoteList(@LoginMember AuthMember member) {
-    var result = getVoteUseCase.getVoteSummaryList(member.getMemberId());
+  public ResponseEntity<?> getVoteList() {
+    var result = getVoteUseCase.getVoteSummaryList(0L);
     return JsonResponse.okWithData(HttpStatus.OK, "투표 전체 조회", result);
   }
 
   @GetMapping("/{votingId}")
-  public ResponseEntity<?> getVoteSummaryResponse(@PathVariable Long votingId, @LoginMember AuthMember member) {
-    var result = getVoteUseCase.getVoteSummary(votingId, member.getMemberId());
+  public ResponseEntity<?> getVoteSummaryResponse(@PathVariable Long votingId) {
+    var result = getVoteUseCase.getVoteSummary(votingId, 0L);
     return JsonResponse.okWithData(HttpStatus.OK, "투표 세부 사항 조회", result);
   }
 
   @PostMapping("")
-  public ResponseEntity<?> vote(@RequestBody RegisterVoterRequest voterRequest, @LoginMember AuthMember member) {
-    registerVoterUseCase.registerVoter(voterRequest.toCommand(member.getMemberId()));
+  public ResponseEntity<?> vote(@RequestBody RegisterVoterRequest voterRequest) {
+    registerVoterUseCase.registerVoter(voterRequest.toCommand(0L));
     return JsonResponse.ok(HttpStatus.CREATED, "투표 성공");
   }
-
 
   @GetMapping(value = "/connect/{votingId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
   public SseEmitter connect(@PathVariable Long votingId) {
 
-    //멤버가 투표를 안했으면 에러 처리해야 함.
+    if(getVoterUseCase.isVoted(votingId, 0L)){
+      throw new RuntimeException("이미 투표했습니다.");
+    }
 
     SseEmitter emitter = new SseEmitter(60 * 1000L);
     sseEmitters.add(votingId, emitter);
 
     try {
       emitter.send(SseEmitter.event()
-          .name("SHOW_VOTE_RESULT_"+votingId)
+          .name("SHOW_VOTE_RESULT_" + votingId)
           .data("connected!\n\n"));
     } catch (IOException e) {
       throw new RuntimeException(e);
--- src/main/java/seoultech/startapp/global/config/security/JwtAuthenticationFilter.java
+++ src/main/java/seoultech/startapp/global/config/security/JwtAuthenticationFilter.java
@@ -77,6 +77,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     skipPathList.add(new AntPathRequestMatcher("/api/version", HttpMethod.GET.name()));
     skipPathList.add(new AntPathRequestMatcher("/api/v1/food-trucks", HttpMethod.GET.name()));
     skipPathList.add(new AntPathRequestMatcher("/api/v1/photo-zones", HttpMethod.GET.name()));
+    skipPathList.add(new AntPathRequestMatcher("/api/v1/**", HttpMethod.GET.name()));
+    skipPathList.add(new AntPathRequestMatcher("/api/v1/**", HttpMethod.POST.name()));
+    skipPathList.add(new AntPathRequestMatcher("/api/v1/**", HttpMethod.OPTIONS.name()));
     OrRequestMatcher orRequestMatcher = new OrRequestMatcher(new ArrayList<>(skipPathList));
     return skipPathList.stream()
         .anyMatch(p -> orRequestMatcher.matches(request));
