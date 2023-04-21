6fb57b1ed49410fb087a2f7e608d74f467fbef49
유서린 <sprout3082@gmail.com>
2023-04-21 18:11:51 +0900

    refactor : 축제 기간 수정하기

--- src/main/java/seoultech/startapp/festival/application/FestivalTimeChecker.java
+++ src/main/java/seoultech/startapp/festival/application/FestivalTimeChecker.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class FestivalTimeChecker {
 
-  private final LocalDateTime FESTIVAL_START_TIME = LocalDateTime.of(2022, 9, 21, 9, 0);
-  private final LocalDateTime FESTIVAL_END_TIME = LocalDateTime.of(2022,9,24,0,0);
+  private final LocalDateTime FESTIVAL_START_TIME = LocalDateTime.of(2023, 5, 10, 9, 0);
+  private final LocalDateTime FESTIVAL_END_TIME = LocalDateTime.of(2023,5,12,0,0);
 
   public boolean isStart(LocalDateTime current) {
     if (current.isBefore(FESTIVAL_START_TIME)) {
